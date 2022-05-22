7d3cd0eac1bec691932a4992181299c9cd7fb365
Jyotendra <jyotendrachandan02@gmail.com>
2022-05-22 22:37:15 +0530

    Advance pattern completion

---
+++ Pattern Advance/zig-zag-pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"No of stars: ";
+    cin>>n;
+    for(int i = 1; i <=3;i++)
+    {
+        for(int j = 1; j<= n; j++)
+        {
+            if(i%2==0 && (i+j)%2==0)
+            cout<<"* ";
+            else if(i%2!=0 && (i+j)%4==0)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
